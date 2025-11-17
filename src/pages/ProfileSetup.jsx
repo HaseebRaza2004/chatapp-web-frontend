c656ccd3b96aa314d957d2793f0ff17a1d996755
Muhammad Haseeb Raza <haseebraza2004@gmail.com>
2025-11-17 16:40:17 +0500

    Fix case-sensitive path issues

--- src/pages/ProfileSetup.jsx
+++ src/pages/ProfileSetup.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Logo from "../components/Logo";
-import ProfileSetupForm from "../components/forms/ProfileSetupForm";
+import ProfileSetupForm from "../components/forms/ProfileSetupFormTEMP";
 
 export default function ProfileSetup() {
   const navigate = useNavigate();
